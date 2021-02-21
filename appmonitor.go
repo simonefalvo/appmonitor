414cd63fc612daec583186ef1750d77e953ca76f
smvfal <smvfal@gmail.com>
2021-02-21 16:02:35 +0100

    remove stacking defer, refactoring

--- appmonitor.go
+++ appmonitor.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 	"time"
-    "strconv"
-    "strings"
 
 	"github.com/prometheus/client_golang/api"
 	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
@@ -14,27 +14,27 @@ import (
 
 func main() {
 
-    appname , ok := os.LookupEnv("APPLICATION_NAME")
-    if !ok {
-        fmt.Printf("Application name not set\n")
-    }
-    prometheus, ok := os.LookupEnv("PROMETHEUS_HOSTNAME")
-    if !ok {
-        fmt.Printf("Prometheus hostname not set\n")
-    }
-    port, ok := os.LookupEnv("PROMETHEUS_PORT")
-    if !ok {
-        fmt.Printf("Prometheus port not set\n")
-    }
-    period, ok := os.LookupEnv("QUERY_PERIOD")
-    if !ok {
-        fmt.Printf("Query period not set\n")
-    }
-    period_seconds, err := strconv.Atoi(period)
-    if err != nil{
-        fmt.Printf("Error converting query period seconds\n")
-        os.Exit(1)
-    }
+	appName, ok := os.LookupEnv("APPLICATION_NAME")
+	if !ok {
+		fmt.Printf("Application name not set\n")
+	}
+	prometheus, ok := os.LookupEnv("PROMETHEUS_HOSTNAME")
+	if !ok {
+		fmt.Printf("Prometheus hostname not set\n")
+	}
+	port, ok := os.LookupEnv("PROMETHEUS_PORT")
+	if !ok {
+		fmt.Printf("Prometheus port not set\n")
+	}
+	period, ok := os.LookupEnv("QUERY_PERIOD")
+	if !ok {
+		fmt.Printf("Query period not set\n")
+	}
+	periodSeconds, err := strconv.Atoi(period)
+	if err != nil {
+		fmt.Printf("Error converting query period seconds\n")
+		os.Exit(1)
+	}
 
 	client, err := api.NewClient(api.Config{
 		Address: "http://" + prometheus + ":" + port,
@@ -46,24 +46,24 @@ func main() {
 
 	v1api := v1.NewAPI(client)
 
-    var b strings.Builder
-    fmt.Fprintf(&b, "rate(gateway_functions_seconds_sum{function_name='%s'}[20s]) / ", appname)
-	fmt.Fprintf(&b, "rate(gateway_functions_seconds_count{function_name='%s'}[20s])", appname)
+	var b strings.Builder
+	fmt.Fprintf(&b, "rate(gateway_functions_seconds_sum{function_name='%s'}[20s]) / ", appName)
+	fmt.Fprintf(&b, "rate(gateway_functions_seconds_count{function_name='%s'}[20s])", appName)
 	query := b.String()
-    fmt.Println("Query: " + query)
+	fmt.Println("Query: " + query)
 
-    for {
-        ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-        defer cancel() //TODO: avoid stacking
-        result, warnings, err := v1api.Query(ctx, query, time.Now())
-        if err != nil {
-            fmt.Printf("Error querying Prometheus: %v\n", err)
-            os.Exit(1)
-        }
-        if len(warnings) > 0 {
-            fmt.Printf("Warnings: %v\n", warnings)
-        }
-        fmt.Printf("Result:\n%v\n", result)
-        time.Sleep(time.Duration(period_seconds) * time.Second)
-    }
+	for {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		result, warnings, err := v1api.Query(ctx, query, time.Now())
+		if err != nil {
+			fmt.Printf("Error querying Prometheus: %v\n", err)
+			os.Exit(1)
+		}
+		if len(warnings) > 0 {
+			fmt.Printf("Warnings: %v\n", warnings)
+		}
+		fmt.Printf("Result:\n%v\n", result)
+		go cancel()
+		time.Sleep(time.Duration(periodSeconds) * time.Second)
+	}
 }
